3787a8642bde27a244429d2ccd0c7f60e2d32d1f
Marc Lasson <marc.lasson@gmail.com>
2018-12-27 17:32:55 +0100

    Chilling

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,13 +1,130 @@
 
+interface Control {
+    root : HTMLElement;
+}
+
+class FormControl implements Control {
+    private form : HTMLFormElement; 
+
+    constructor() {
+        this.form = document.createElement('form');
+        this.form.className = "container";
+    }
+
+    public get root() {
+        return this.form;
+    } 
+
+    public add(e : Control) {
+        this.form.appendChild(e.root)
+    }
+}
+
+
+class RangeControl implements Control {
+    private static id : number = 0;
+    private _label : string; 
+    private callbacks :  ((input : number) => void) [] = [];
+
+    private elements : {
+        div : HTMLDivElement;
+        input : HTMLInputElement;
+        label : HTMLLabelElement;
+    };
+
+    constructor () {
+        RangeControl.id ++;
+        var this_id = 'formControlRange' + RangeControl.id;
+        this.elements = {
+            div : document.createElement('div'),
+            input : document.createElement('input'),
+            label : document.createElement('label'),
+        };
+        this.elements.div.appendChild(this.elements.label);
+        this.elements.div.appendChild(this.elements.input);
+        this.elements.div.className = 'form-group';
+        this.elements.input.id = this_id;
+        this.elements.input.className = 'form-control-range';
+        this.elements.input.type = 'range';
+        this.elements.label.htmlFor = this_id;
+        this.elements.input.oninput = () => { 
+            for (let f of this.callbacks) {
+                f(this.value);
+            };
+        }
+    }
+
+    public get value() : number {
+        return Number(this.elements.input.value);
+    }
+
+    public set value(x: number) {
+        this.elements.input.value = String(x);
+    }
+
+    public get label(): string {
+		return this._label;
+	}
+
+	public set label(value: string) {
+        console.log(value);
+        this._label = value;
+        this.elements.label.textContent = value;
+    }
+    
+    public get root() {
+        return this.elements.div;
+    }
+
+    public onChange (cb: (input : number) => void) {
+        this.callbacks.push(cb);
+    }
+
+}
+
+class State {
+    x : RangeControl;
+    y : RangeControl;
+    z : RangeControl;
+    constructor (x : RangeControl, y : RangeControl, z : RangeControl) {
+        this.x = x;
+        this.y = y; 
+        this.z = z;
+    }
+}
+
+function createState(): State {
+    let form = new FormControl();    
+    let x = new RangeControl();
+    let y = new RangeControl();
+    let z = new RangeControl();
+
+    document.body.appendChild(form.root);
+    form.add(x);
+    form.add(y);
+    form.add(z);
+
+    x.value = 0.0;
+    y.value = 0.0; 
+    z.value = 0.0;
+    x.label = "x";
+    y.label = "y";
+    z.label = "z";
+
+    return new State(x,y,z);
+}
+
+
 class Game {
+    private _renderCallbacks : (() => void) [];
     private _canvas: HTMLCanvasElement;
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
-    private _camera: BABYLON.FreeCamera;
+    private _sphere: BABYLON.Mesh;
+    private _camera: BABYLON.ArcRotateCamera;
     private _lights: Array<BABYLON.Light>;
 
     constructor(canvasElement : string) {
-        // Create canvas and engine.
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
         this._engine = new BABYLON.Engine(this._canvas, true);
     }
@@ -17,33 +134,38 @@ class Game {
         this._scene = new BABYLON.Scene(this._engine);
 
         // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 1, -10), this._scene);
-
+        this._camera = new BABYLON.ArcRotateCamera('camera1', 0, 0, 10, new BABYLON.Vector3(0, 0, 0), this._scene);
+        
         // Target the camera to scene origin.
         this._camera.setTarget(BABYLON.Vector3.Zero());
 
         // Attach the camera to the canvas.
-        this._camera.attachControl(this._canvas, false);
+        this._camera.attachControl(this._canvas, false, true);
 
         this._lights = [];
         this._lights.push(new BABYLON.PointLight('light1', new BABYLON.Vector3(0,10,0), this._scene));
         this._lights.push(new BABYLON.PointLight('light2', new BABYLON.Vector3(5,10,0), this._scene));
 
         // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
-        let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
+        this._sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
                                 {segments: 16, diameter: 2}, this._scene);
 
         // Move the sphere upward 1/2 of its height.
-        sphere.position.y = 0;
+        this._sphere.position.y = 0;
 
         // Create a built-in "ground" shape.
         let ground = BABYLON.MeshBuilder.CreateGround('ground1',
                                 {width: 6, height: 6, subdivisions: 2}, this._scene);
     }
 
-    doRender() : void {
+    doRender(state : State) : void {
         // Run the render loop.
         this._engine.runRenderLoop(() => {
+            if (this._sphere) {
+              this._sphere.position.x = state.x.value;
+              this._sphere.position.y = state.y.value;
+              this._sphere.position.z = state.z.value;
+            }
             this._scene.render();
         });
 
@@ -55,12 +177,8 @@ class Game {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Create the game using the 'renderCanvas'.
     let game = new Game('renderCanvas');
-
-    // Create the scene.
+    let state = createState();
     game.createScene();
-
-    // Start render loop.
-    game.doRender();
+    game.doRender(state);
 });
